5658c412c4bd609a807dd13154b033e991f38a4f
Harvard-Westlake <atheiss@hw.com>
2021-09-02 09:51:07 -0700

    Duplicate depost

--- src/ATM.java
+++ src/ATM.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class ATM {
 	private HashMap<String, Double> accounts = new HashMap<String, Double>();
-	
+
 	public void deposit (double amount, String id) {
 		if (accounts.containsKey(id)) {
 			accounts.put(id, (double)accounts.get(id) + amount);
@@ -10,5 +10,13 @@ public class ATM {
 			accounts.put(id, amount);
 		}
 	}
-	
+
+	public void deposit2 (double amount, String id) {
+		if (accounts.containsKey(id)) {
+			accounts.put(id, (double)accounts.get(id) + amount);
+		}
+		else {
+			accounts.put(id, amount);
+		}
+	}
 }
